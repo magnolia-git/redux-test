7c7bee354eafdc293aadb08cee9c284ea993dd8e
Luis Ibanez <luisangelibanez@gmail.com>
2021-02-10 22:03:16 -0600

    update reducer.js to update the jwt in order for it to be saved ln11, updated action to only let users with correct credentials log in ln61

--- src/redux/users/actions.js
+++ src/redux/users/actions.js
@@ -1,72 +1,73 @@
-import * as Actions from './action-types';
+import * as Actions from "./action-types";
 
 export const addToken = (token) => ({
-    type: Actions.ADD_TOKEN,
-    payload: token,
+  type: Actions.ADD_TOKEN,
+  payload: token,
 });
 
 // Registering a user
 export const createUser = (newUser) => (dispatch) => {
-    const URL = "http://localhost:8080/api/authenticate/createUser";
-    return fetch(URL, {
-        method: "POST",
-        body: JSON.stringify(newUser),
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        }
+  const URL = "http://localhost:8080/api/authenticate/createUser";
+  return fetch(
+    URL,
+    {
+      method: "POST",
+      body: JSON.stringify(newUser),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
     },
-        (error) => {
-            var errmess = new Error(error.message);
-            throw errmess;
-        }
-    )
-        .then((response) => response.json())
-        .then((response) => {
+    (error) => {
+      var errmess = new Error(error.message);
+      throw errmess;
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log({ response });
+      alert(JSON.stringify(response));
 
-
-            console.log({ response });
-            alert(JSON.stringify(response));
-
-            return; // dispatch(addUser(response))
-
-        })
-        .catch((error) => {
-            console.log("Submit user ", error.message);
-            alert("User cannot be registered\nError: " + error.message);
-        });
+      return; // dispatch(addUser(response))
+    })
+    .catch((error) => {
+      console.log("Submit user ", error.message);
+      alert("User cannot be registered\nError: " + error.message);
+    });
 };
 
 // Logging in as a user
 export const postUser = (returningUser) => (dispatch) => {
-
-//    const URL = "http://localhost:8080/api/authenticate";
-    const URL = "http://192.168.4.11:31337/authenticate";
-    return fetch(URL, {
-        method: "POST",
-        body: JSON.stringify(returningUser),
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-        }
+  const URL = "http://localhost:8080/api/authenticate";
+  //    const URL = "http://192.168.4.11:31337/authenticate";
+  return fetch(
+    URL,
+    {
+      method: "POST",
+      body: JSON.stringify(returningUser),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
     },
-        (error) => {
-            var errmess = new Error(error.message);
-            throw errmess;
-        }
-    )
-        .then((response) => response.json())
-        .then((response) => {
-          const jwtToken = response.jwtToken;
-          if (jwtToken !== null) {
-            localStorage.setItem('jwt', jwtToken);
-            dispatch(addToken(response));
-          }
-
-
-        })
-        .catch((error) => {
-            console.log("Submit user ", error.message);
-            alert("User cannot sign in\nError: " + error.message);
-        });
+    (error) => {
+      var errmess = new Error(error.message);
+      throw errmess;
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      const jwtToken = response.jwt; //Update store to show message to user whenever login is unsuccessful
+      if (jwtToken !== undefined) {
+        localStorage.setItem("jwt", jwtToken);
+        dispatch(addToken(response));
+      } else {
+        console.log("failed to login");
+      }
+    })
+    .catch((error) => {
+      console.log("Submit user ", error.message);
+      alert("User cannot sign in\nError: " + error.message);
+    });
 };
--- src/redux/users/reducer.js
+++ src/redux/users/reducer.js
@@ -1,16 +1,14 @@
-import * as Actions from './action-types';
+import * as Actions from "./action-types";
 
 const initialState = {
-    jwt: undefined,
-    isLoggedIn: false
-}
+  jwt: undefined,
+  isLoggedIn: false,
+};
 
 export function reducer(state = initialState, action) {
-
-  switch(action.type) {
-
+  switch (action.type) {
     case Actions.ADD_TOKEN:
-      return {...state, isLoggedIn: true, token: localStorage.jwt };
+      return { ...state, isLoggedIn: true, jwt: action.payload.jwt };
 
     default:
       return state;
